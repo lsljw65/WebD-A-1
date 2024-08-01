2aa9a24efe6edc39d993b298cb4be5a1de4b9380
lsljw65 <48504421+lsljw65@users.noreply.github.com>
2024-08-01 17:36:50 +0900

    서브메뉴 기능구현

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
 $(function () {
+  // 서브 메뉴
+  $(".sub-menu").each(function (index) {
+    $(this).mouseenter(function () {
+      $("nav>ul>li>a").removeClass("active");
+      $("nav>ul>li>a").eq(index).addClass("active");
+    });
+    $(this).mouseleave(function () {
+      $("nav>ul>li>a").removeClass("active");
+    });
+  });
+  $("nav").mouseleave(function () {
+    $(".sub-menu-back").stop().slideUp();
+    $(".sub-menu").stop().slideUp();
+  });
+  $("nav").mouseenter(function () {
+    $(".sub-menu-back").stop().slideDown();
+    $(".sub-menu").stop().slideDown();
+  });
+
   //   tab구현
   $(".tab-content").hide();
   $(".tab-content").eq(0).show();
